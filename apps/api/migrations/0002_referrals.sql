707e12b4c90b0e4fc3663e373ecb514746b33628
Elliot Braem <elliot@ejlbraem.com>
2025-02-26 15:07:17 -0700

    successful migration

--- apps/api/migrations/0002_referrals.sql
+++ apps/api/migrations/0002_referrals.sql
@@ -1,4 +1,2 @@
--- Migration number: 0002 	 2025-02-26T21:50:00.000Z
-
 ALTER TABLE users ADD COLUMN referred_by VARCHAR(42) REFERENCES users(id) ON DELETE CASCADE;
 CREATE INDEX idx_users_referred_by ON users(referred_by);
