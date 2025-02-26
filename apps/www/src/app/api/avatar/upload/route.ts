9b08bd1857b769136aee43f100331fadb7d222ef
Elliot Braem <elliot@ejlbraem.com>
2025-02-26 15:29:52 -0700

    fmt

--- apps/www/src/app/api/avatar/upload/route.ts
+++ apps/www/src/app/api/avatar/upload/route.ts
@@ -1,38 +1,35 @@
-import { put } from '@vercel/blob';
-import { NextResponse } from 'next/server';
+import { put } from "@vercel/blob";
+import { NextResponse } from "next/server";
 
-export const runtime = 'edge';
+export const runtime = "edge";
 
 export async function POST(request: Request): Promise<NextResponse> {
   try {
     const { searchParams } = new URL(request.url);
-    const filename = searchParams.get('filename');
+    const filename = searchParams.get("filename");
 
     if (!filename) {
       return NextResponse.json(
-        { error: 'No filename provided' },
-        { status: 400 }
+        { error: "No filename provided" },
+        { status: 400 },
       );
     }
 
     if (!request.body) {
-      return NextResponse.json(
-        { error: 'No file provided' },
-        { status: 400 }
-      );
+      return NextResponse.json({ error: "No file provided" }, { status: 400 });
     }
 
     const blob = await put(filename!, request.body, {
-      access: 'public',
+      access: "public",
       addRandomSuffix: true,
     });
 
     return NextResponse.json(blob);
   } catch (error) {
-    console.error('Error uploading file:', error);
+    console.error("Error uploading file:", error);
     return NextResponse.json(
-      { error: 'Error uploading file' },
-      { status: 500 }
+      { error: "Error uploading file" },
+      { status: 500 },
     );
   }
 }
